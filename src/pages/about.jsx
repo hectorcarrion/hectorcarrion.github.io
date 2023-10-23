7a0e5c1681d090dafefc6d9791e81b70d5d40bc8
Hector Carrion <43754045+hectorcarrion@users.noreply.github.com>
2023-10-23 00:07:44 -0400

    Update about.jsx

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -204,7 +204,7 @@ export default function About() {
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-                I’m Héctor Carrión - crazy about AI 4 good ✌🏼
+                I’m Héctor - crazy about AI for good ✌🏼
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>

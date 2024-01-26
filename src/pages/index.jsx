d6616fc419fc56f8ce8cdcf532a084dbc65cd067
Hector Carrion <43754045+hectorcarrion@users.noreply.github.com>
2024-01-25 22:15:20 -0400

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -254,7 +254,7 @@ export default function Home({ articles }) {
           CS PhD at UCSC, Advisor
           @ <a href="https://tera.earth" target="_blank" className="hover:underline" rel="noopener noreferrer">tera.earth</a>
           &nbsp;& <a href="https://visia.ai" target="_blank" className="hover:underline" rel="noopener noreferrer">visia.ai</a>
-          &nbsp;- Multi-modal, self-supervised AI reserach for real-world positive impact 🌱
+          &nbsp;- I drive multi-modal, self-supervised AI reserach for real-world positive impact 🌱
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink

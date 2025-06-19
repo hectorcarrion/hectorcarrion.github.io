0fe42f63f7546318cb4e7a091c80101137384b0a
Hector Carrion <yakk@Hectors-MacBook-Pro.local>
2025-06-19 11:17:29 -0400

    show all papers on homepage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -300,7 +300,6 @@ export async function getStaticProps() {
   return {
     props: {
       articles: (await getAllArticles())
-        .slice(0, 4)
         .map(({ component, ...meta }) => meta),
     },
   }

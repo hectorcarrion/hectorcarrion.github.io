03b61f8c108959aa4c7906709f9d3989011c15e3
Hector Carrion <43754045+hectorcarrion@users.noreply.github.com>
2025-09-26 10:56:42 -0400

    Update resume link to September 2025 version

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -161,7 +161,7 @@ function Resume() {
           </li>
         ))}
       </ol>
-      <Button href="https://github.com/hectorcarrion/hectorcarrion.github.io/blob/main/Hector%20Carrion%20Resume%20Jun%202025.pdf?raw=true" variant="secondary" className="group mt-6 w-full">
+      <Button href="https://github.com/hectorcarrion/hectorcarrion.github.io/blob/main/Hector%20Carrion%20Resume%20Sep%202025.pdf?raw=true" variant="secondary" className="group mt-6 w-full">
         Download Resume
         <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
       </Button>
